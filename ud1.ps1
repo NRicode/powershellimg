0193f283dff802ef87f14d94a27143b8781d5902
Marvin E <106366188+NRicode@users.noreply.github.com>
2024-01-03 16:11:29 +0700

    Update and rename kkg.ps1 to ud1.ps1

---
+++ ud1.ps1
@@ -0,0 +1,21 @@
+# Load the required assembly for Windows Forms
+Add-Type -AssemblyName System.Windows.Forms
+
+# Load the required assembly for handling media
+Add-Type -AssemblyName PresentationCore
+
+# Create a form
+$form = New-Object Windows.Forms.Form
+$form.Text = "Sound Player"
+$form.Width = 400
+$form.Height = 100
+
+$MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
+$MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://nyanpass.com/nyanpass.mp3')
+$MediaPlayer.Play()
+
+$form.ShowDialog()
+
+# Clean up resources
+$webClient.Dispose()
+$form.Dispose()
