5de06dcd4d3844ddfef8fd820de4eb1aeea23cb2
Marvin E <106366188+NRicode@users.noreply.github.com>
2024-01-03 21:36:47 +0700

    Update g5bvcd.ps1

--- g5bvcd.ps1
+++ g5bvcd.ps1
@@ -1,7 +1,21 @@
 $MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
 $MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://nyanpass.com/nyanpass.mp3')
 Write-Host "Hello, World!"
+
+# Register an event handler for the MediaEnded event
+$MediaEndedEvent = Register-ObjectEvent -InputObject $MediaPlayer -EventName MediaEnded -Action {
+    # This code will be executed when the MediaEnded event is triggered
+    Write-Host "Media playback completed."
+    
+    # Clean up the event subscription
+    Unregister-Event -SourceIdentifier $Event.SourceIdentifier
+}
+
+# Start playing the media
 $MediaPlayer.Play()
+
+# Wait for the MediaEnded event
+Wait-Event -SourceIdentifier $MediaEndedEvent.SourceIdentifier | Out-Null
 
 # Clean up resources
 #$MediaPlayer.Dispose()
