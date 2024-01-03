ca46de08f832d1f60595bcb93c368a999712a040
Marvin E <106366188+NRicode@users.noreply.github.com>
2024-01-03 15:54:15 +0700

    Create g.ps1

---
+++ g.ps1
@@ -0,0 +1,38 @@
+# Load the required assembly
+Add-Type -AssemblyName System.Windows.Forms
+
+# Create a form
+$form = New-Object Windows.Forms.Form
+$form.Text = "Audio Player"
+$form.Width = 800
+$form.Height = 600
+
+# Create a sound player
+$soundPlayer = New-Object System.Media.SoundPlayer
+
+# Specify the URL of the audio file
+$audioUrl = "https://cdn.pixabay.com/download/audio/2022/03/09/audio_fb0098c6da.mp3?filename=emergency-alarm-with-reverb-29431.mp3"
+
+# Download the audio file from the URL
+$webClient = New-Object System.Net.WebClient
+$audioData = $webClient.DownloadData($audioUrl)
+
+# Create a temporary file to save the audio data
+$tempFilePath = [System.IO.Path]::GetTempFileName() + ".wav"
+[IO.File]::WriteAllBytes($tempFilePath, $audioData)
+
+# Set the temporary file as the audio source for the sound player
+$soundPlayer.SoundLocation = $tempFilePath
+
+# Play the audio
+$soundPlayer.Play()
+
+# Clean up resources when the form is closed
+$form.Add_FormClosed({
+    $soundPlayer.Dispose()
+    $webClient.Dispose()
+    Remove-Item -Path $tempFilePath -ErrorAction SilentlyContinue
+})
+
+# Show the form
+$form.ShowDialog()
