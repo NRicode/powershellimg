c279495445fa4953f0e088be983a33f5d3a27219
Marvin E <106366188+NRicode@users.noreply.github.com>
2024-01-03 21:29:38 +0700

    Update and rename cdcc.ps1 to cdccc.ps1

--- cdccc.ps1
+++ cdccc.ps1
@@ -1,6 +1,7 @@
 $MediaPlayer = [Windows.Media.Playback.MediaPlayer, Windows.Media, ContentType = WindowsRuntime]::New()
 $MediaPlayer.Source = [Windows.Media.Core.MediaSource]::CreateFromUri('https://nyanpass.com/nyanpass.mp3')
+Write-Host "Hello, World!"
 $MediaPlayer.Play()
 
 # Clean up resources
-#MediaPlayer.Dispose();
+MediaPlayer.Dispose();
