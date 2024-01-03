04ea26c993cafd58a8f51b69f53c44d055c23a76
Marvin E <106366188+NRicode@users.noreply.github.com>
2024-01-03 15:59:44 +0700

    Update and rename g3.ps1 to gcc3.ps1

--- gcc3.ps1
+++ gcc3.ps1
@@ -8,12 +8,16 @@ $form.Width = 800
 $form.Height = 600
 
 $soundUrl = "https://cdn.pixabay.com/download/audio/2022/03/09/audio_fb0098c6da.mp3?filename=emergency-alarm-with-reverb-29431.mp3"
-$webClient = New-Object System.Net.WebClient
-$soundBytes = $webClient.DownloadData($soundUrl)
 
-$stream = New-Object System.IO.MemoryStream($soundBytes)
+$webClient = New-Object System.Net.WebClient
+$soundData = $webClient.DownloadData($soundUrl)
+
+$memoryStream = New-Object System.IO.MemoryStream
+$memoryStream.Write($soundData, 0, $soundData.Length)
+$memoryStream.Seek(0, [System.IO.SeekOrigin]::Begin)  # Reset the stream position to the beginning
+
 $soundPlayer = New-Object System.Media.SoundPlayer
-$soundPlayer.Stream = $stream
+$soundPlayer.Stream = $memoryStream
 $soundPlayer.Play()
 
 # Clean up resources when the form is closed
